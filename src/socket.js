3eed9d9d3f8ee93cc5f6f4c4eef69bfa36370ee3
facundoiba95 <facundoiba95@gmail.com>
2023-07-25 12:22:33 -0300

    change url in socket.io

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io('https://vps-3471494-x.dattaweb.com/api/chat-socketio');
+const socket = io('https://vps-3471494-x.dattaweb.com/api/chatsocketio');
 
 export default socket;
